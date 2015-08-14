271ed847da75f2bbd4a563ab3f65eab360085ce2
rwatts3 <rwatts@SV-WS-DEV-116.220OFFICE.local>
2015-08-14 11:21:41 -0700

    initial commit

---
+++ Air Droid/config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Air Droid",
+   "homepage": "http://web.airdroid.com/",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": false
+};
---
+++ Google Sheets/config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Google Sheets",
+   "homepage": "https://docs.google.com/spreadsheets/u/0/",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": false
+};
